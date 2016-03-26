63386235747ceda4056b9e94c6dc964d721e79d5
Thomas Smits <thomsmits@gmail.com>
2016-03-26 23:09:29 +0100

    Aligned Windows and Unix structure.
    Now the directories used for compiled and temporary results are the same
    on both platforms.

--- latex/clean.cmd
+++ latex/clean.cmd
@@ -1,11 +1,10 @@
 @echo off
-SET RESULT_DIR=01_Thesis
-SET OUTPUT_DIR=00_Output
+SET RESULT_DIR=..\result
+SET OUTPUT_DIR=..\temp
 SET FILE=thesis
 cd tex
 del /S /Q %OUTPUT_DIR%
 rmdir %OUTPUT_DIR%
-cd ..
 del /S /Q %RESULT_DIR%
 rmdir %RESULT_DIR%
-DIR%
+cd ..
--- latex/create.cmd
+++ latex/create.cmd
@@ -1,14 +1,14 @@
 @echo off
-SET RESULT_DIR=01_Thesis
-SET OUTPUT_DIR=00_Output
+SET RESULT_DIR=..\result
+SET OUTPUT_DIR=..\temp
 SET FILE=thesis
-mkdir %RESULT_DIR%
 cd tex
+mkdir %RESULT_DIR%
 mkdir %OUTPUT_DIR%
 pdflatex -output-directory=%OUTPUT_DIR% -synctex=1 %FILE%.tex 
 bibtex %OUTPUT_DIR%\%FILE%.aux
 makeindex %OUTPUT_DIR%\%FILE%.idx
 pdflatex -output-directory=%OUTPUT_DIR% -synctex=1 %FILE%.tex
 pdflatex -output-directory=%OUTPUT_DIR% -synctex=2 %FILE%.tex
-copy %OUTPUT_DIR%\%FILE%.pdf ..\%RESULT_DIR%\%FILE%.pdf
+copy %OUTPUT_DIR%\%FILE%.pdf %RESULT_DIR%\%FILE%.pdf
 cd ..
